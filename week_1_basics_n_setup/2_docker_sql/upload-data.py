ba109df211a2cbe930873dfcc241dc3074ce875e
Ben <ben@popandpeek.com>
2023-01-23 20:03:08 +0000

    changes data file, adds gitignore

---
+++ week_1_basics_n_setup/2_docker_sql/upload-data.py
@@ -0,0 +1,67 @@
+import os
+import argparse
+
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_zones = pd.read_csv('taxi+_zone_lookup.csv')
+
+    df_zones.to_sql(name='zones', con=engine, if_exists='replace')
+
+
+    df_iter = pd.read_csv('green_tripdata_2019-01.csv', iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    while True: 
+        try:
+            t_start = time()
+
+            df = next(df_iter)
+
+            df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+            df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+            
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+
+            t_end = time()
+
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+            
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+
+    args = parser.parse_args()
+
+    main(args)
